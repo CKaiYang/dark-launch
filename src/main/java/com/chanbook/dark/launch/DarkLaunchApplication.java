c40f5e616f803533220ed467c19cdde563c3ec11
chenkaiyang <648715275@qq.com>
2021-03-05 17:32:33 +0800

    init

---
+++ src/main/java/com/chanbook/dark/launch/DarkLaunchApplication.java
@@ -0,0 +1,13 @@
+package com.chanbook.dark.launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkLaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkLaunchApplication.class, args);
+    }
+
+}
